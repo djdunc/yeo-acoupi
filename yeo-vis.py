31be58a8ff182ca97bd817e0f8c40a0f5683d1ee
Duncan Wilson <drdunc@gmail.com>
2026-08-29 11:01:04 +0100

    refactor: clean up biodiversity map HTML and sync with Python visualizer updates

--- yeo-vis.py
+++ yeo-vis.py
@@ -587,6 +587,22 @@ def render_sensor_map(loc_df, key_suffix=""):
                 padding: 0;
             }}
         </style>
+        <!-- Matomo -->
+        <script>
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {{
+                var u="https://matomo.cetools.org/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '9']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            }})();
+        </script>
+        <noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="https://matomo.cetools.org/matomo.php?idsite=9&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+        <!-- End Matomo Code -->
     </head>
     <body>
         <div id="map"></div>
